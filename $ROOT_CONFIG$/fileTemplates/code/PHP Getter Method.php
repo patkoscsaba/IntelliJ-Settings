2f657bd8384134f8048963866831abfe61e9d27c
Patkos Csaba <csaba@feedxl.com>
2015-08-17 08:42:50 +0300

    <csaba@sabayon Get rid of $ROOT_CONFIG$

--- $ROOT_CONFIG$/fileTemplates/code/PHP Getter Method.php
+++
@@ -1,8 +0,0 @@
-public ${STATIC} function get${NAME}()
-{
-#if (${STATIC} == "static")
-    return self::$${FIELD_NAME};
-#else
-    return $this->${FIELD_NAME};
-#end
-}
--- $ROOT_CONFIG$/fileTemplates/code/PHP Setter Method.php
+++
@@ -1,8 +0,0 @@
-public ${STATIC} function set${NAME}($${PARAM_NAME})
-{
-#if (${STATIC} == "static")
-    self::$${FIELD_NAME} = $${PARAM_NAME};
-#else
-    $this->${FIELD_NAME} = $${PARAM_NAME};
-#end
-}
